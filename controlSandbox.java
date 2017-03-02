0e6d6e18d314b0dd62025d0b8743ff51b9834e2a
Nik Earnest <nikolasjearnest@gmail.com>
2017-03-01 23:07:33 -0800

    MVC Update

--- controlSandbox.java
+++ controlSandbox.java
@@ -12,22 +12,27 @@ public class controlSandbox {
 		{
 			if(input == 1)
 			{
+				//Calls the VIEW to print out information for user interaction
 				view.printText("Enter a number you want to save: ");
+				
+				// calls to VIEW to read in the input
 				input = view.getInputInt();
+				
+				//Checks the input with the data in the MODEL 
 				if(!model.checkInput(input))
 				{model.setNumber(input);
-				view.printText("Success /n Data:");
-				view.printText(model.getData());}
+				view.printText("Success /n Data:");//if success sends success message to view.
+				view.printText(model.getData());}//Prints out all data from the MODEL
 				else
 				{
-					view.printText("Number already exists in Data");
+					view.printText("Number already exists in Data");//Error Message sent to view
 				}
 			}
 			else
 			{
-				view.printText(model.getData());
+				view.printText(model.getData());// Calls view to print out data. 
 			}
-			view.printText("1: Add Number /n 2:View Data /n 3:Quit");
+			view.printText("1: Add Number /n 2:View Data /n 3:Quit"); //prints out menu to view
 			input = view.getInputInt();
 		}
 	}
--- modelSandbox.java
+++ modelSandbox.java
@@ -6,14 +6,17 @@ import java.util.Set;
 public class modelSandbox {
 
 		static Set<Integer> allData = new HashSet<Integer>();
+		//Setter for entering a number
 		public void  setNumber(Integer in)
 		{
 			allData.add(in);
 		}
+		//check input checks if the input is already in the hash set
 		public static boolean checkInput(Integer in)
 		{
 			return allData.contains(in);
 		}
+		//Getter to return full data set
 		public static String getData()
 		{
 			return allData.toString();
